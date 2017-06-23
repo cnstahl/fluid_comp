610c33fdc0c2022a3a40eacc4f99c94063584c61
Charles Stahl <cnstahl@princeton.edu>
2017-06-23 17:16:06 -0400

    joining vtk files

--- tutorials/blast_mpi/join.sh
+++ tutorials/blast_mpi/join.sh
@@ -1,6 +1,6 @@
-for i in `seq 10 11`;
+for i in `seq 0 9`;
 do
 #    echo $i
-    ./join_vtk++ -o Blast.000$i.vtk Blast.block0.out1.000$i.vtk Blast.block1.out1.000$i.vtk Blast.block2.out1.000$i.vtk Blast.block3.out1.000$i.vtk \
-	Blast.block4.out1.000$i.vtk Blast.block5.out1.000$i.vtk Blast.block6.out1.000$i.vtk Blast.block7.out1.000$i.vtk 
+    ./join_vtk++ -o Blast.0000$i.vtk Blast.block0.out1.0000$i.vtk Blast.block1.out1.0000$i.vtk Blast.block2.out1.0000$i.vtk Blast.block3.out1.0000$i.vtk \
+	Blast.block4.out1.0000$i.vtk Blast.block5.out1.0000$i.vtk Blast.block6.out1.0000$i.vtk Blast.block7.out1.0000$i.vtk 
 done 
