292ffea94010a591af9a11542e1b12784f09358b
Charles Stahl <cnstahl@princeton.edu>
2017-07-31 14:34:23 -0400

    new data

---
+++ sound_waves/no_diff32/decay_rate.py
@@ -0,0 +1,47 @@
+import glob
+import numpy as np
+import athena_read
+
+print "make sure to use python2"
+
+def mag(data, type_):
+    if type_ == 'vel':
+        return np.linalg.norm(data)
+    if type_ == 'press' or type_ == 'rho':
+        return data
+    else: raise ValueError
+
+names = glob.glob('*.vtk')
+nfiles = len(names)
+
+name = str(names[0])
+pos_dot = name.index('.')
+name = name[:pos_dot]
+
+presss = np.zeros(nfiles)
+vels   = np.zeros(nfiles)
+rhos   = np.zeros(nfiles)
+for n in range(nfiles):
+    print("-------", n, "------")
+    filename = "%s.%04i.vtk" %(name, n)
+    x_faces,y_faces,z_faces,data = athena_read.vtk(filename)
+    press = data['press']
+    vel = data['vel']
+    rho = data['rho']
+    max_press = 0
+    max_vel   = 0
+    max_rho   = 0
+    
+    for i in range(len(z_faces)-1):
+        for j in range(len(y_faces)-1):
+            for k in range(len(x_faces)-1):
+                max_press = max(max_press, mag(press[i,j,k], 'press'))
+                max_vel   = max(max_vel  , mag(vel[i,j,k],   'vel'))
+                max_rho   = max(max_rho  , mag(rho[i,j,k],   'rho'))
+    presss[n] = max_press
+    vels[n]   = max_vel
+    rhos[n]   = max_rho
+
+np.savetxt('presss', presss)
+np.savetxt('vels', vels)
+np.savetxt('rhos', rhos)
